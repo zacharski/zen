27b77158ecd9989574fc2af63faea37e7393e888
Ron Zacharski <ron.zacharski@gmail.com>
2024-12-14 16:23:51 -0700

    hosting trial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [preact()]
+  site: 'https://zacharski.github.io',
+  base: 'my-repo/zen',
+
+});
