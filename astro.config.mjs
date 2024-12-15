15b3bac89fb9f471e6423e7ed6038d2317d63ad8
Ron Zacharski <ron.zacharski@gmail.com>
2024-12-14 17:25:11 -0700

    fixing yaml

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact()],
   site: 'https://zacharski.github.io',
   base: '/zen',
 
